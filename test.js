7f019508acdf1137df798e8d1eb0c47b6a3ce187
nata388 <128762188+nata388@users.noreply.github.com>
2023-08-04 21:12:08 +0000

    dolartoyen

---
+++ test.js
@@ -0,0 +1,30 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+
+}
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+}
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromDollarToYen(1.2)).toBe(127.9); //1 dolar son 1.2 Yen, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromYenToPound(127.9)).toBe(0.8); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
